e79a666258a5718c545a3318e7a76bf040f3e5b2
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-04-01 00:53:51 +0100

    Read library UUID from command line.

--- src/main/scala/me/m1key/audioliciousmigration/Launcher.scala
+++ src/main/scala/me/m1key/audioliciousmigration/Launcher.scala
@@ -6,11 +6,23 @@ object Launcher {
   def main(args: Array[String]): Unit = {
     println("Audiolicious Importer")
     
+    val libraryUuid = resolveLibraryUuid(args)
+    println("Migrating library [%s].".format(libraryUuid))
+    
     val injector = Guice.createInjector(new AudioliciousMigrationModule)
     val importer = injector.getInstance(classOf[AudioliciousImporter])
     importer.importLibrary("UUID")
     
     println("Bye.")
+  }
+  
+  def resolveLibraryUuid(args: Array[String]): String = {
+    if (args.length > 0) {
+      return args(0)
+    } else {
+      println("Library UUID not specified. Obtaining most recent library...")
+      return "TODO"
+    }
   }
 
 }
